4a28a89f84e008fdac1f0b0085efe79c8cf34dfd
ravgusha <ravgusha@gmail.com>
2023-05-09 22:51:25 +0300

    feat: add like button block

---
+++ src/blocks/like-button/like-button.js
@@ -0,0 +1,31 @@
+const likeBtns = document.getElementsByClassName("like-button");
+
+const likeClick = (e) => {
+  const likeBtn = e.target.parentElement;
+  let numberOfLikesElement = likeBtn.querySelector(".like-button__count");
+  let numberOfLikes = Number.parseInt(numberOfLikesElement.textContent, 10);
+
+  let isLiked;
+
+  if (likeBtn.classList.contains("isLiked")) {
+    isLiked = true;
+  } else isLiked = false;
+
+  if (!isLiked) {
+    likeBtn.classList.add("isLiked");
+    numberOfLikes++;
+    numberOfLikesElement.textContent = numberOfLikes;
+    isLiked = !isLiked;
+  } else {
+    likeBtn.classList.remove("isLiked");
+    numberOfLikes--;
+    numberOfLikesElement.textContent = numberOfLikes;
+    isLiked = !isLiked;
+  }
+  console.log(isLiked);
+};
+console.log(likeBtns);
+
+Array.from(likeBtns).forEach((element) => {
+  element.addEventListener("click", likeClick);
+});
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./fonts/fonts.scss";
 import "./main.scss";
 
 import './blocks/input/input';
+import './blocks/like-button/like-button';
